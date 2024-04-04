6c90b66f2dcd58a45e9a62b9424e934eddd45580
zademdws <marcusengsoft@gmail.com>
2024-04-04 16:14:59 -0300

    atv3

---
+++ preparação para HTML e CSS/arrays3.py
@@ -0,0 +1,9 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 
+                  'loções', 'xampus', 'sabonetes', 'delineadores']
+
+for i in range(len(lista_produtos)):
+    
+    produto = lista_produtos[i]
+    
+    print(f"Temos {produto} à venda!")
+
