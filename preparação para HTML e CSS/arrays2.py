99567428dc2eaa83caea933a024fbac2e0a6f24a
zademdws <marcusengsoft@gmail.com>
2024-04-04 06:06:13 -0300

    atv2

---
+++ preparação para HTML e CSS/arrays2.py
@@ -0,0 +1,9 @@
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 
+                 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 
+                 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 
+                 'Yamandú Costa', 'Gal Costa'] 
+
+print(len(lista_musicos))
+print(lista_musicos[2])
+print(lista_musicos[9])
+print(lista_musicos[14])
